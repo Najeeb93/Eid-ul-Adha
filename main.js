ed565dc5b7a544ff8306dece379a4f6848895686
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-05-08 13:40:41 +0500

    add to commit upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 var nowdate = new Date().getTime();
 afterEid = new Date("15 june, 2024").getTime();
 var leftTime = afterEid - nowdate
-var month = Math.floor(leftTime / (1000 * 60 * 60 * 60 * 24) % 31);
+var month = Math.floor(leftTime / (1000 * 60 * 60 * 24 * 31));
 var day = Math.floor(leftTime / (1000 * 60 * 60 * 60) % 24);
 var hour = Math.floor(leftTime / (1000 * 60 * 60) % 60);
 var minute = Math.floor(leftTime / (1000 * 60) % 60);
